0627264df064da34d9e02a8186b44192821db8f0
andrewgspeed03 <aa.chen.goodspeed@gmail.com>
2024-03-15 14:17:47 -0500

    added mod client and description

---
+++ src/main/java/net/andrewcg/cweastermod/CWEasterModClient.java
@@ -0,0 +1,10 @@
+package net.andrewcg.cweastermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CWEasterModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
