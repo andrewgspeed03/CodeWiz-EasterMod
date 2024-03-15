daec5e65d3b41789f6f7bfc90493070c56a94f3d
andrewgspeed03 <aa.chen.goodspeed@gmail.com>
2024-03-15 14:28:01 -0500

    fixed error with logger

--- src/main/java/net/andrewcg/cweastermod/CWEasterMod.java
+++ src/main/java/net/andrewcg/cweastermod/CWEasterMod.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class CWEasterMod implements ModInitializer {
 
 	public static final String MOD_ID = "cw-eastermod";
-    public static final Logger LOGGER = LoggerFactory.getLogger();
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
